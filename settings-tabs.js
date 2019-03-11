bf1b5d094a95f403cde23ffca9549c1e6edfc82b
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2019-03-11 20:38:38 +0600

    u

--- settings-tabs.js
+++ settings-tabs.js
@@ -20,6 +20,26 @@ jQuery(document).ready(function($){
 
 	})
 
+
+
+
+    $(document).on('click', '.settings-tabs .expandable .expand', function()
+    {
+        if($(this).parent().parent().hasClass('active'))
+        {
+            $(this).parent().parent().removeClass('active');
+        }
+        else
+        {
+            $(this).parent().parent().addClass('active');
+        }
+
+
+    })
+
+
+
+
 	
  		
 
